82bb6c2c7b71bc7a754bb76719087156829f5986
Wordless Echo <me@wordlessecho.com>
2019-02-17 14:15:55 +0800

    Disable HTTP while HTTPS is enabled

--- config.sample.js
+++ config.sample.js
@@ -1,10 +1,9 @@
 config = {
   mongodb_server_url: 'mongodb://127.0.0.1:27017/ptpush',
-  server_http_port: 3000,
-  server_https_port: 3001,
-  enable_https: true,
+  server_port: 3000,
+  /* enable_https: true,
   private_key_path: './cert/private.pem',
-  certificate_path: './cert/file.crt'
+  certificate_path: './cert/file.crt' */
 }
 
 module.exports = config;
--- server.js
+++ server.js
@@ -24,15 +24,9 @@ mongoose.connect(DB_URL, { useMongoClient: true }).then(
         app.use('/subscribe', subscribeapi);
         app.use('/', queryapi);
 
-        let httpServer = Http.createServer(app);
-        httpServer.listen(process.env.PORT || Config.server_http_port, () => {
-            let host = httpServer.address().address;
-            let port = httpServer.address().port;
-
-            console.log("Http Server running at http://%s:%s", host, port);
-
-            tasks.scheduleQueryTask();
-        });
+        if (!Config.server_port) {
+          let server_port = process.env.PORT;
+        }
 
         if (Config.enable_https) {
           console.log("Https Server is enabled. Now loading credentials...");
@@ -47,7 +41,16 @@ mongoose.connect(DB_URL, { useMongoClient: true }).then(
 
             console.log("Https Server running at https://%s:%s", host, port);
           });
+        } else {
+          let httpServer = Http.createServer(app);
+          httpServer.listen(Config.server_http_port, () => {
+              let host = httpServer.address().address;
+              let port = httpServer.address().port;
+
+              console.log("Http Server running at http://%s:%s", host, port);
+          });
         }
+        tasks.scheduleQueryTask();
     },
     err => { console.log(err.message) }
 );
